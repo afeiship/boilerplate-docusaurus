09469a7d967ed116afeaf0c511749d1616db469e
afeiship <1290657123@qq.com>
2020-06-10 09:16:52 +0800

    refactor: nx.param udpate

--- src/next-node-fetch.js
+++ src/next-node-fetch.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
   var NxDataTransform = require('@feizheng/next-data-transform');
@@ -12,35 +12,35 @@
   var NxNodeFetch = nx.declare('nx.NodeFetch', {
     properties: {
       config: {
-        set: function(inConfig) {
+        set: function (inConfig) {
           this._config = nx.mix(null, DEFAULT_OPTIONS, inConfig);
         },
-        get: function() {
+        get: function () {
           return this._config;
         }
       }
     },
     statics: {
-      request: function(inUrl, inMethod, inData, inOptions) {
+      request: function (inUrl, inMethod, inData, inOptions) {
         var options = nx.mix(null, DEFAULT_OPTIONS, this.config, inOptions);
         var isGET = inMethod === 'get';
         var body = isGET ? null : NxDataTransform[options.dataType](inData);
-        var url = isGET ? inUrl + '?' + nxParam(inData) : inUrl;
+        var url = isGET ? nxParam(inData, inUrl) : inUrl;
         var headers = { 'Content-Type': nxContentType(options.dataType) };
         var config = nxDeepAssign({ method: inMethod, body: body, headers: headers }, options);
-        var responseHandler = function(res) {
+        var responseHandler = function (res) {
           return options.responseType ? res[options.responseType]() : res;
         };
 
         return options.delay
           ? options
-              .fetch(url, config)
-              .then(nxDelay(options.delay))
-              .then(responseHandler)
+            .fetch(url, config)
+            .then(nxDelay(options.delay))
+            .then(responseHandler)
           : options.fetch(url, config).then(responseHandler);
       },
-      'get,delete,head,post,put,patch': function(inMethod) {
-        return function(inUrl, inData, inOptions) {
+      'get,delete,head,post,put,patch': function (inMethod) {
+        return function (inUrl, inData, inOptions) {
           return this.request(inUrl, inMethod, inData, inOptions);
         };
       }
